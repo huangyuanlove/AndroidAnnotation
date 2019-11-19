2040c2bbfcf54ccc9ae6813c5344b87e345c9217
huangyuan <huangyuan@chunyu.me>
2019-11-19 13:43:47 +0800

    校验权限

--- view-inject-api/src/main/java/com/huangyuanlove/view_inject_api/PermissionUtil.java
+++ view-inject-api/src/main/java/com/huangyuanlove/view_inject_api/PermissionUtil.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import androidx.collection.SimpleArrayMap;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
 /**
  * 1. 先调用hasSelfPermissions，有权限则直接执行
@@ -21,7 +22,7 @@ public class PermissionUtil {
     private static final SimpleArrayMap<String, Integer> MIN_SDK_PERMISSIONS;
 
     static {
-        MIN_SDK_PERMISSIONS = new SimpleArrayMap<String, Integer>(8);
+        MIN_SDK_PERMISSIONS = new SimpleArrayMap<>(8);
         MIN_SDK_PERMISSIONS.put("com.android.voicemail.permission.ADD_VOICEMAIL", 14);
         MIN_SDK_PERMISSIONS.put("android.permission.BODY_SENSORS", 20);
         MIN_SDK_PERMISSIONS.put("android.permission.READ_CALL_LOG", 16);
@@ -95,7 +96,7 @@ public class PermissionUtil {
 
     private static boolean hasSelfPermission(Context context, String permission) {
         try {
-            return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+            return PermissionChecker.checkSelfPermission(context, permission) == PermissionChecker.PERMISSION_GRANTED;
         } catch (RuntimeException t) {
             return false;
         }
